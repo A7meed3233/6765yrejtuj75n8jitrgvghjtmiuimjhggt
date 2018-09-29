a3ec09c7f41a92c923f22412e9e9eb1f11ae607e
A7meed3233 <43696377+A7meed3233@users.noreply.github.com>
2018-09-29 13:40:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,6 +37,23 @@ client.on("guildMemberAdd", member => {
 }).catch(console.error)
 })
 
+client.on('message', message => {
+    var args = message.content.split(/[ ]+/)
+    if(message.content.includes('discord.gg')){
+        message.delete()
+      message.channel.sendMessage("", {embed: {
+        title: "لا تنشر",
+        color: 0x06DF00,
+        description: "يمنع النشر في هذا السيرفر",
+        footer: {
+          text: "By A7med"
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
+
+     
+}); 
+
 
 
 client.on('message', message => {
