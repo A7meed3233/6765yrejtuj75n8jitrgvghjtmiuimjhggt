3a9dff13d14034febdc8c2d6efe461069401590d
A7meed3233 <43696377+A7meed3233@users.noreply.github.com>
2018-10-03 18:40:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,6 +76,29 @@ if(message.content.startsWith(prefix + 'hackban')) {
 
 
 
+
+
+
+client.on('message', message => {
+let perm = message.guild.member(message.author).hasPermission('ADMINISTRATOR') || message.guild.member(message.author).hasPermission('BAN_MEMBERS')
+if (!perm) return message.reply(':x: | **You don\'t have `BAN_MEMBERS` permission to use this command**.')
+if(message.content.startsWith(prefix + 'unhackban')) {
+  let nourid = message.content.split(" ").slice(3).join(" ");
+  let nour = bot.fetchUser(nourid)
+  .then(user => {
+    message.guild.unban(user.id)
+    .then(() => {
+      message.channel.send(`Alright, I unhackbanned ${user}.`)
+    }).catch(err => {
+        message.channel.send(`Failed to unban :( ${user}`)
+    })
+  }).catch(() => message.channel.send("Theres no user with the this ID :face_palm:"))
+}
+  })
+
+
+
+
 client.on('message', message => {
     if (message.author.id === client.user.id) return;
     if (message.guild) {
