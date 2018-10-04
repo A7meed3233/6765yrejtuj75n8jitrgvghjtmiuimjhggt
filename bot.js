321a8a4a4b5c39ff51f3e1471899cd7c6453cc21
A7meed3233 <43696377+A7meed3233@users.noreply.github.com>
2018-10-04 23:32:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,6 +46,18 @@ if (message.content.startsWith("S!cv")) {
 
 
 
+client.on("message", (message) => {
+if (message.content.startsWith("S!ct")) {
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+        let args = message.content.split(" ").slice(1);
+    message.guild.createChannel(args.join(' '), 'text');
+message.channel.sendMessage('تـم إنـشاء روم كـتابـي')
+
+}
+});
+
+
+
 
 
 const credits = JSON.parse(fs.readFileSync("./creditsCode.json", "utf8"));
