cbc883374b80b4c0b7b74fb61b8bc10252b1a118
A7meed3233 <43696377+A7meed3233@users.noreply.github.com>
2018-10-04 15:55:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -106,6 +106,19 @@ client.on("message", (message) => {
 
 
 
+client.on("guildCreate", guild => {
+    client.channels.get("495536963554902027").send(' ***  BOT  ***   **Join To**   ***[ ' + `${guild.name}` + ' ]***   ,   **  Owner  **  ' + ' ***[ ' + '<@' + `${guild.owner.user.id}` + '>' + ' ]***  **|**  ***[ ' + '<' + `${guild.owner.user.username}` + '>' + ' ]***')
+    });
+    
+    client.on("guildDelete", guild => {
+    client.channels.get("495536963554902027").send(' ***  BOT  ***   **Leave From**   ***[ ' + `${guild.name}` + ' ]***   ,   **  Owner  **  ' + ' ***[ ' + '<@' + `${guild.owner.user.id}` + '>' + ' ]***  **|**  ***[ ' + '<' + `${guild.owner.user.username}` + '>' + ' ]***')
+    });
+
+
+
+
+
+
 
 
 
