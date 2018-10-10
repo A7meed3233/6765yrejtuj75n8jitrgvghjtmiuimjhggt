263a0e184dcb286f3f8fd58365a65b70cb4e5986
A7meed3233 <43696377+A7meed3233@users.noreply.github.com>
2018-10-11 00:54:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,6 +30,37 @@ client.user.setGame(`Sooooon..!`,"http://twitch.tv/S-F")
 });
 
 
+
+
+const Discord = require("discord.js")
+const client = new Discord.Client();
+const YTDL = require('ytdl-core');
+const nodeopus = require('node-opus');
+const ffmpeg = require('ffmpeg');
+var servers = {};
+function play(connection, message, args) {
+  var server = servers[message.guild.id];
+  server.dispatcher = connection.playStream(YTDL(args[0]), {filter: "audioonly"});
+  server.queue.shift();
+  server.dispatcher.on("end", function() {
+    if (server.queue[0]) play(connection, message);
+    else connection.disconnect();
+  });
+}
+
+
+client.on('message', message =>{
+  if(message.content.startsWith('Sjoin')){
+    const voiceChannel = message.member.voiceChannel
+    voiceChannel.join();
+    message.channel.send("تم الأتصال بالروم الصوتي")
+}})
+
+
+
+
+
+
 client.on('message', message => {
 	
 	if(message.author.bot) return;
