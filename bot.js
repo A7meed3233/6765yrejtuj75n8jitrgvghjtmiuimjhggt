77f5a745d5fdd742093cff8b2b68fb6774b718e1
A7meed3233 <43696377+A7meed3233@users.noreply.github.com>
2018-10-13 11:50:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,35 +37,6 @@ client.user.setGame(`Sooooon..!`,"http://twitch.tv/S-F")
 
 
 
-var math = require('mathjs') // npm i mathjs
-client.on("message", async msg => {
-
-    if (msg.channel.type !== "text") return undefined;
-
-    //if (msg.auhtor.bot) return undefined;
-
-    var args = msg.content.split(" ")
-
-    var prefix = "S!"
-
-  if (msg.content.toLowerCase().startsWith(prefix + "cal")) {
-
-    if (!args[1]) return msg.channel.send("DiscordAPI Err : Missing args.")
-
-    if (args[1].length == 1) return msg.channel.send("JUST ONE NUMBER?????");
-
-    var count = parseInt(args[1]);
-
-    if (isNaN(count)) return msg.channel.send('No nigga');
-
-    try {
-      idk = await math.eval(args[1])
-    } catch (e) {
-      return msg.channel.send("ERRRRRRRRRRRRRRRRRRRRRRRR")
-    }
-    await msg.channel.send(idk)
-  }
-});
 
 
 
