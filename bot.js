95d14bb43643f00aa6fd088d1484e083b9ba1cca
A7meed3233 <43696377+A7meed3233@users.noreply.github.com>
2018-10-15 14:14:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,20 @@
+const convert = require("hh-mm-ss")
+const dateFormat = require('dateformat');
+const fs = require('fs');
+const pretty = require('pretty-ms');
+const rn = require('random-number');
+const userData = JSON.parse(fs.readFileSync('./userData.json', 'utf8'));
+const moment = require('moment');
+var Canvas = require('canvas')
+var jimp = require('jimp')
+const prefix = "S!";
+let done = {};
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.setMaxListeners(100)
-const fs = require("fs"); 
-const moment = require('moment');
-const jimp = require('jimp');
-const Canvas = require('canvas');
 client.on('ready', () => {
-console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Soon`,"http://twitch.tv/S-F")
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`S!help | S!invite`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -29,6 +36,35 @@ client.user.setGame(`Soon`,"http://twitch.tv/S-F")
   console.log('╚[════════════]╝')
   console.log('')
   console.log('')
+});
+
+
+
+
+
+ client.on('message', message => {
+	 var prefix ="S!";
+ if(message.content.startsWith(prefix +"server")){
+if(!message.channel.guild) return message.reply(' ');
+const millis = new Date().getTime() - message.guild.createdAt.getTime();
+const now = new Date();
+dateFormat(now, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
+const verificationLevels = ['None', 'Low', 'Medium', 'Insane', 'Extreme'];
+const days = millis / 1000 / 60 / 60 / 24;
+let roles = client.guilds.get(message.guild.id).roles.map(r => r.name);
+var embed  = new Discord.RichEmbed()
+.setAuthor(message.guild.name, message.guild.iconURL)
+.addField("**🆔 Server ID:**", message.guild.id,true)
+.addField("**📅 Created On**", message.guild.createdAt.toLocaleString(),true)
+.addField("**👑 Owned by**",`${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`)
+.addField("👥 Members ",`[${message.guild.memberCount}]`,true)
+.addField('**💬 Channels **',`**${message.guild.channels.filter(m => m.type === 'text').size}**` + ' text | Voice  '+ `**${message.guild.channels.filter(m => m.type === 'voice').size}** `,true)
+.addField("**🌍 Others **" , message.guild.region,true)
+.addField("** 🔐 Roles **",`**[${message.guild.roles.size}]** Role `,true)
+.setColor('#000000')
+message.channel.sendEmbed(embed)
+
+}
 });
 
 
