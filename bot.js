2ebdc05b3854300489e38a592b8fcd0b9e3b695f
A7meed3233 <43696377+A7meed3233@users.noreply.github.com>
2018-10-17 16:17:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -913,7 +913,7 @@ client.on('ready', () => {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
     let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "The Royal Legend";
+    let copy = "${message.guild.name}";
     let request = `Requested By ${message.author.username}`;
     if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
     msg.react('✅')
@@ -934,7 +934,6 @@ client.on('ready', () => {
        .addField('Server | سيرفر', message.guild.name)
        .addField('Sender | المرسل', message.author.username)
        .addField('Message | الرسالة', args)
-       .setThumbnail(message.author.avatarURL)
        .setFooter(copy, client.user.avatarURL);
     m.send({ embed: bc })
     msg.delete();
