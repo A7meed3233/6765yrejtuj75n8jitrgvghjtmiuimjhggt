f452f7684a7ec6be1b52a7edec79658e92bd8f82
A7meed3233 <43696377+A7meed3233@users.noreply.github.com>
2018-10-18 22:01:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -242,8 +242,7 @@ channel.guild.owner.send(`<@!${channelremover.id}>
   });
 
 
-const Discord = require ('discord.js');
-const bot = new Discord.Client();
+
 console.log ("CONNECTING...");
  
 var gEnteredUsers = [];
