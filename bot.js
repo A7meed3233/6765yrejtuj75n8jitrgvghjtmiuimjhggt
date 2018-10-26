a028209e55dcb3e6b1ce757a4a5bbb718f30dd16
A7meed3233 <43696377+A7meed3233@users.noreply.github.com>
2018-10-26 23:00:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2604,18 +2604,14 @@ client.on('message', message => {
 
 
 
-const Discord = require('discord.js');
 const db = require('quick.db');
-const client = new Discord.Client();   
 const giphy = require('giphy-api')();    
 const googl = require('goo.gl');  
 const translate = require('google-translate-api');   
-const fs = require("fs"); 
 const getYoutubeID = require('get-youtube-id'); 
 const moment = require("moment");  
 const { Client, Util } = require('discord.js');  
 const UserBlocked = new Set(); 
-const jimp = require('jimp');   
 const math = require('math-expression-evaluator'); 
 const stripIndents = require('common-tags').stripIndents;
 const figlet = require('figlet');
