cb8dd8d5d606095efb2d7b0cbb06122c8547374e
A7meed3233 <43696377+A7meed3233@users.noreply.github.com>
2018-10-27 22:42:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const prefix = 'S!'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`S!help | S!invite` `${client.guilds.size} Servers !`,"http://twitch.tv/S-F")
+client.user.setGame(`S!help | S!invite`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
