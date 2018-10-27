f9f8e63d7370002d25244448b2ff756698768896
A7meed3233 <43696377+A7meed3233@users.noreply.github.com>
2018-10-27 18:34:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const prefix = 'S!'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Soon`,"http://twitch.tv/S-F")
+client.user.setGame(`S!help | S!invite`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -2184,43 +2184,49 @@ var prefix = "S!";
 
 
 
-client.on("message", async message => {
-        if(!message.channel.guild) return;
- var prefix= "S!";
-        if(message.content.startsWith(prefix + 'server')) {
-		  if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
- 
-        let guild = message.guild
-        let channel = message.channel
-        let guildicon = guild.icon_url
-        let members = guild.memberCount
-        let bots = guild.members.filter(m => m.user.bot).size
-        let humans = members - bots
-        let allchannels = guild.channels.size
-        let textchannels = guild.channels.filter(e => e.type === "text")
-        let voicechannels = guild.channels.filter(e => e.type === "voice")
-          var embed = new Discord.RichEmbed()
-          .setColor("#000000")
-          .setTitle(`معلومات عن السيرفر`)
-          .setDescription(`معلومات عن : ${guild.name}`)
-          .addField("صاحب السيرفر :", `${guild.owner}`, true)
-          .addField("أيدي السيرفر :", `${guild.id}`, true)
-          .addField("موقع السيرفر :", `${guild.region}`, true)
-          .addField("مستوى حماية السيرفر :", `${guild.verificationLevel}`, true)
-          .addField("عدد الرومات الصوتية :", `${voicechannels.size}`, true)
-          .addField("عدد الرومات الكتابية :", `${textchannels.size}`, true)
-          .addField("عدد اعضاء السيرفر :", `${members}`, true)
-          .addField("عدد البوتات :", `${bots}`, true)
-          .addField("عدد الاشخاص :", `${humans}`, true)
-          .addField("عدد رتب السيرفر :", `${guild.roles.size}`, true)
-          .addField(`أيموجيات الخاصة بالسيرفر : (${guild.emojis.size})`, `- ${guild.emojis.array()}`, true)
-          .setFooter(`تم انشاء هذه السيرفر في: ${guild.createdAt}`)
- 
-       message.channel.send({ embed: embed });
- 
-      }
-    });
+client.on('message', function(message) {
+    if(message.content.startsWith (prefix  + 'server')) {
+          let guild = message.guild;
+  let icon = message.guild.iconURL;
+  let createdAtRaw = guild.createdAt.toDateString();
+  let createdAt = createdAtRaw.split(" ");
+  let bots = message.guild.members.filter(m => m.user.bot).size;
+  let humans = message.guild.members.filter(m => !m.user.bot).size;
+  let channels = message.guild.channels.size;
+  let textChannels = message.guild.channels.filter(m => m.type == "text").size;
+  let voiceChannels = message.guild.channels.filter(i => i.type == "voice").size;
+  let emojis = [];
+  guild.emojis.forEach(emoji => {
+  emojis.push(`\`${emoji}\``);
+  });
+  emojis.length === 0 ? emojis = "None" : emojis = emojis.join(", ");
 
+  let roles = [];
+  guild.roles.forEach(role => {
+    roles.push(`\`${role.name}\``);
+  });
+  roles = roles.join(", ");
+
+  let embed = new Discord.RichEmbed()
+  .setTitle(`Server Stats`)
+  .setThumbnail(icon)
+  .addField('Guild Name', guild.name, true)
+  .addField('Guild ID', guild.id, true)
+  .addField('Guild Owner', `${guild.owner.user.tag}`, true)
+  .addField('Created At', `${createdAt[0]} ${createdAt[2]} ${createdAt[1]} ${createdAt[3]}`, true)
+  .addField('Region', guild.region.toUpperCase(), true)
+  .addField('Total Members:', guild.memberCount, true)
+  .addField('Bots:', bots, true)
+  .addField('Users:', humans, true)
+  .addField('Verification Level', guild.verificationLevel, true)
+  .addField('Text Channels', textChannels, true)
+  .addField('Voice Channels', voiceChannels, true)
+  .addField(`Roles`, `${guild.roles.size}`, true)
+  .addField(`Emojis`, `${guild.emojis.size}`, true)
+
+      message.channel.send({embed:embed});
+    }
+  });
 
 
 
